c8ed8a4cd76e66898c91fe6ed02b0a6076e4c139
plgagne <pierrelucgagne@icloud.com>
2023-01-02 11:33:29 -0500

    tabs

--- book-binding/image-sizing.sh
+++ book-binding/image-sizing.sh
@@ -4,6 +4,6 @@ mkdir "temp"
 mkdir "temp/assets"
 cp -r "../assets/" "temp/assets/"
 find "temp/assets" -type f -name '*.png' -print0 | while IFS= read -r -d '' line; do
-echo $line
-magick mogrify -interpolate nearest -filter point -resize 800% $line
+    echo $line
+    magick mogrify -interpolate nearest -filter point -resize 800% $line
 done
