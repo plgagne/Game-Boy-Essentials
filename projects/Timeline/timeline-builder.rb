da2005f67ded30b911ef59c38878effe6fe33954
Pierre-Luc Gagné <1243344+plgagne@users.noreply.github.com>
2023-01-15 05:33:32 +0000

    ajustements des dates

--- projects/Timeline/timeline-builder.rb
+++ projects/Timeline/timeline-builder.rb
@@ -1,21 +1,24 @@
 #!/usr/bin/ruby
 file = File.open("temp/timeline-temp.yaml", "rb")
 contents = file.read
+
 # Remove every instance of two spaces
 contents = contents.gsub(/  /, "")
+
 # Remove all tabs
 contents = contents.gsub(/\t/, "")
+
 # Remove break spaces
 contents = contents.gsub(/<br\ \/>/, "")
 contents = contents.gsub(/\r/, "")
+
 # Merge lines
 contents = contents.gsub(/>\n/, ">")
-# title and representative boxart
 
+# title and representative boxart
 contents = contents.gsub(/<header\ class=\"page-header\">(.*?)background-image:url\(https:\/\/gamefaqs\.gamespot\.com\/(.*?)\.jpg(.*?)page-title\">(.*?)\ &ndash;(.*?)\n/, " - game:\n  representative name: \"\\4\"\n  representative boxart: \"\\2.jpg\"\n\\5")
 
 # related version
-
 contents = contents.gsub(/ Release Details(.*?)<span class="header_more(.*?)">/,"  related version:\n")
 contents = contents.gsub(/<span class="header_more" style="cursor: pointer">/, "")
 contents = contents.gsub(/<i class="fa fa-caret-down">(.*?)<a href="https:\/\/gamefaqs.gamespot.com\/(.*?)"><span class="also_name">(.*?)<\/span><\/a>/, "\n    - \"\\3\"\n")
@@ -25,7 +28,6 @@ contents = contents.gsub(/<\/div><\/span><\/h3>(.*?)<\/nav>/, "")
 contents = contents.gsub(/<\/span><\/h3>(.*?)<\/nav>/, "")
 
 # genre
-
 contents = contents.gsub(/&gt; /, "&gt;")
 contents = contents.gsub(/ &gt;/, "&gt;")
 contents = contents.gsub(/<div\ class=\"pod\ pod_titledata\">(.*?)Genre:<\/dt><dd>(.*?)&gt;/, "  genre: [\"\\2&gt;")
@@ -33,29 +35,24 @@ contents = contents.gsub(/&gt;(.*?)/, "\", \"\\1")
 contents = contents.gsub(/genre:(.*?)\n/, "genre:\\1\"]\n")
 
 # developer
-
 contents = contents.gsub(/<\/dd><dt>Developer:<\/dt><dd>(.*?)\n/, "  developers:\n<\/dd><dt>Developer:<\/dt><dd>\\1")
 contents = contents.gsub(/<\/dd><dt>Developer:<\/dt><dd><a(.*?)>(.*?)<\/a>(.*?)/, "    - developer: \"\\2\"\n\\3")
 
 # ESRB move
-
 contents = contents.gsub(/<\/dd><dt>ESRB Descriptor\(s\):<\/dt><dd>(.*?)<\/dd>(.*?)\n/, "\\2\n{ESRB descriptors: \"\\1\"}")
 contents = contents.gsub(/<\/dd><dt>ESRB Descriptor\(s\):<\/dt><dd>(.*?)\n/, "\n{ESRB descriptors: \"\\1\"}")
 contents = contents.gsub(/{(.*?)}<\/dd>(.*?)\n/, "\\2\n{\\1}")
 
 # players
-
 contents = contents.gsub(/<\/dd><dt>Local Players:<\/dt><dd>(.*?)<\/dd><dt>/, "  players: \"\\1\"\n")
 contents = contents.gsub(/<(.*?)>Local Players:<\/dt><dd>(.*?)\n/, "  players: \"\\2\"\n")
 
 # wikipedia
-
 contents = contents.gsub(/Wikipedia \(EN\):<\/dt><dd><a target="_blank" href=(.*?)>(.*?)<\/a>.*?/, "  wikipedia: \\1\n")
 contents = contents.gsub(/{(.*?)}<(.*?)>Wikipedia \(EN\):<\/dt><dd><a target="_blank" href=(.*?)>(.*?)<\/a>.*?/, "  wikipedia: \\3\n{\\1}")
 contents = contents.gsub(/<(.*?)>Wikipedia \(EN\):<\/dt><dd><a target="_blank" href=(.*?)>(.*?)<\/a>.*?/, "  wikipedia: \\2\n")
 
 # releases
-
 contents = contents.gsub(/<\/dd><\/dl><\/div><\/div>\n/, "")
 contents = contents.gsub(/{(.*?)}(.*?)<td class="cregion">US<\/td>(.*?)\n/, "\\2<td class=\"cregion\">US<\/td>{\\1}\\3\n")
 contents = contents.gsub(/<td class="cregion">US<\/td>{(.*?)}(.*?)<td class="cregion">US<\/td>/, "<td class=\"cregion\">US<\/td>{\\1}\\2<td class=\"cregion\">US<\/td>{\\1}")
@@ -67,13 +64,11 @@ contents = contents.gsub(/<\/tbody><\/table><\/div><\/div>/, "")
 contents = contents.gsub(/"&nbsp;"/, "")
 
 # compilation
-
 contents = contents.gsub(/<div class="pod">.*?ompilation.*?<\/colgroup>/, "  compilations:\n")
 contents = contents.gsub(/<tr><td><a href=".*?">(.*?)<\/a><\/td><td>(.*?)<\/td><\/tr>/, "    - compilation:\n      compilation-title: \"\\1\"\n      platform: \"\\2\"\n")
 contents = contents.gsub(/<\/table><\/div><\/div>\n/, "")
 
 # contributor
-
 contents = contents.gsub(/<\/p><\/div><\/div>\n/, "\n")
 contents = contents.gsub(/<div class="pod contrib"><div class="head"><h2 class="title">Contributions.*?\n/, "\n  contributors:")
 contents = contents.gsub(/<a(.*?)<\/a>.*?<a/, "<a\\1<\/a>\n<a")
@@ -81,11 +76,9 @@ contents = contents.gsub(/<a(.*?)<\/a>.*?\n/, "<a\\1<\/a>\n")
 contents = contents.gsub(/<a href=(.*?)>(.*?)<\/a>/, "\n    - contributor:\n      name: \"\\2\"\n      link: \\1")
 
 # description
-
 contents = contents.gsub(/(^[\S])(.*?)\n/, "")
 
 # Cleanup
-
 contents = contents.gsub(/<\/dd>/, "")
 contents = contents.gsub(/_thumb.jpg/, "_front.jpg")
 contents = contents.gsub(/"\.- title: /, "\"\n- title: \n")
@@ -103,5 +96,25 @@ contents = contents.gsub(/Toys "R" Us/, "Toys R Us")
 contents = contents.gsub(/Yu Yu Hakusho "2"/, "Yu Yu Hakusho 2")
 contents = contents.gsub(/Zool: Ninja of the "Nth" Dimension/, "Zool: Ninja of the Nth Dimension")
 contents = contents.gsub(/ It's a breakaway bash in this wild new Kirby game of Block Ball!.*?\n/, "")
+
+# Adjusting the release dates to YYYY/MM/DD
+contents = contents.gsub(/release date: "(.*?)\/(.*?)\/(.*?)"/, "release date: \"19\\3\/\\1\/\\2\"")
+contents = contents.gsub(/release date: "19([0-1][0-9])\/(.*?)\/(.*?)"/, "release date: \"20\\1\/\\2\/\\3\"")
+contents = contents.gsub(/release date: "([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/00\/00\"")
+contents = contents.gsub(/release date: "January ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/01\/00\"")
+contents = contents.gsub(/release date: "February ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/02\/00\"")
+contents = contents.gsub(/release date: "March ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/03\/00\"")
+contents = contents.gsub(/release date: "April ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/04\/00\"")
+contents = contents.gsub(/release date: "May ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/05\/00\"")
+contents = contents.gsub(/release date: "June ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/06\/00\"")
+contents = contents.gsub(/release date: "July ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/07\/00\"")
+contents = contents.gsub(/release date: "August ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/08\/00\"")
+contents = contents.gsub(/release date: "September ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/09\/00\"")
+contents = contents.gsub(/release date: "October ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/10\/00\"")
+contents = contents.gsub(/release date: "November ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/11\/00\"")
+contents = contents.gsub(/release date: "December ([1-2])([0-9])([0-9])([0-9])"/, "release date: \"\\1\\2\\3\\4\/12\/00\"")
+contents = contents.gsub(/release date: \"TBA/, "release date: \"0000")
+contents = contents.gsub(/release date: \"Canceled/, "release date: \"0000")
+
 # Write changes to file
 File.open('results/timeline-complete-data.yaml', "w") {|file| file.puts contents}
