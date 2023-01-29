5d73e12b1e976c91eac2216c3a8a4d40825b450f
Pierre-Luc Gagné <1243344+plgagne@users.noreply.github.com>
2023-01-29 23:37:16 +0000

    mise à jour pour les données réordonnés

--- projects/Timeline/timeline-builder.sh
+++ projects/Timeline/timeline-builder.sh
@@ -1,17 +1,37 @@
 #!/bin/zsh
-# Copy Backup
+backup_location="/Users/plgagne/Documents/Game Boy Essentials Others/timeline project backup"
+
+# Set red for echo output
+RED='\033[0;31m'
+
+# Navigate to correct folder
 cd projects/timeline
+
+# Copy Backup
 if [ -r "temp" ]; then
-	echo "Temporary timeline data is present"
+	echo "${RED}Temporary timeline data is present."
 else 
-	echo "Copying temporary timeline data ..."
+	echo "${RED}Copying temporary timeline data ..."
 	mkdir "temp"
-	cp -R "/Users/plgagne/Documents/Game Boy Essentials Others/timeline project backup" "temp"
-	echo "Done copying."
+	cp -R  "$backup_location"/* "temp"
+	echo "${RED}Done copying."
+fi
+
+# Make resized images 
+if [ -r "../../assets/timeline/a" ]; then
+		echo "${RED}Resized images exist."
+else 
+		echo "${RED}Resized images do not exist. Making them now:"
+		find "temp" -type f -name '*.jpg' -print0 | while IFS= read -r -d '' line; do
+				echo $line
+				magick mogrify $line -resize 160 $line
+		done
+		echo "${RED}Moving resized images to assets folder ..."
+		mv "temp/a" "../../assets/timeline/"
 fi
 
 # Prune data.html files
-echo "Pruning data files ..."
+echo "${RED}Pruning data files ..."
 find "temp" -type f -name 'data.html' -print0 | while IFS= read -r -d '' line; do
 	sed "/<!doctype\ html>/,/<div\ class=\"span8\">/d" "$line" > "$line.tmp"
 	sed -i "" "/<div\ class=\"pod\ contrib\"><div\ class=\"head\"><h2 class=\"title\">Know\ Something\ We\ Don\'t?/,/<\/html>/d" "$line.tmp"
@@ -19,7 +39,7 @@ find "temp" -type f -name 'data.html' -print0 | while IFS= read -r -d '' line; d
 done
 
 # Move data.html files to base
-echo "Moving pruned data files ..."
+echo "${RED}Moving pruned data files ..."
 find "temp" -type f -name '*.tmp' -print0 | while IFS= read -r -d '' line; do
 	dir="$(dirname $line)"   # Returns "/from/here/to"
 	dir="$(basename $dir)"  # Returns just "to"
@@ -27,30 +47,30 @@ find "temp" -type f -name '*.tmp' -print0 | while IFS= read -r -d '' line; do
 done
 
 # Concatenate all files together
-echo "Concatenating pruned data files ..."
+echo "${RED}Concatenating pruned data files ..."
 find "temp" -iname "*.tmp" -type f -exec cat "{}" >> "temp/timeline-temp.yml" \;
 
 # Run Ruby timeline builder
-echo "Running Ruby script ..."
+echo "${RED}Running Ruby script ..."
 ruby timeline-builder.rb
 
 # Remove temporary files
-echo "Removing temporary files ..."
+echo "${RED}Removing temporary files ..."
 find "temp" -iname "*.yml" -type f -exec rm "{}" \;
 find "temp" -iname "*.tmp" -type f -exec rm "{}" \;
 
 # Remove blank lines in YAML file
-echo "Removing blank lines in final file ..."
+echo "${RED}Removing blank lines in final file ..."
 grep '\S' 'results/timeline-complete-data.yml' > 'tmp.txt'
 mv 'tmp.txt' 'results/timeline-complete-data.yml'
 
 # Prepending YAML start of file symbols
-echo "Adding start of file symbols ..."
+echo "${RED}Adding start of file symbols ..."
 echo "---" | cat - 'results/timeline-complete-data.yml' > 'temp/out'
 mv 'temp/out' 'results/timeline-complete-data.yml'
 
 # Make timeline with only the releases
-echo "Running Ruby script for hard releases only ..."
+echo "${RED}Running Ruby script for hard releases only ..."
 ruby "timeline-releases-only.rb"
 
 # Validation check
@@ -58,17 +78,13 @@ yamllint -d "{extends: default, rules: {quoted-strings: enable,line-length: {max
 yamllint -d "{extends: default, rules: {quoted-strings: enable,line-length: {max: 500}}}" "results/timeline-releases-only.yml"
 
 # Compare number of titles
-gameboy_number=$(find "temp/timeline project backup/gamefaqs.gamespot.com/gameboy" -mindepth 1 -maxdepth 1 -type d | wc -l)
-gbc_number=$(find "temp/timeline project backup/gamefaqs.gamespot.com/gbc" -mindepth 1 -maxdepth 1 -type d | wc -l)
+total_number=$(find "temp/games" -mindepth 1 -maxdepth 1 -type d | wc -l)
 timeline_number=$(grep -o "game:" "results/timeline-complete-data.yml" | wc -l)
 releases_number=$(grep -o "release:" "results/timeline-releases-only.yml" | wc -l)
-echo "Number of Game Boy titles in backup:" $gameboy_number
-echo "Number of Game Boy Color titles in backup:" $gbc_number
-total_number=$((gameboy_number+gbc_number))
 echo "Total number of titles in backup:" $total_number
 echo "Total number of titles in YAML file:" $timeline_number
 echo "Total number of releases in hard releases YAML file:" $releases_number
 
 # Move releases-only to _data folder
-echo 'Moving releases only file to _data folder ...'
-cp "results/timeline-releases-only.yml" "../_data/timeline-releases-only.yml"
+echo "${RED}Moving releases only file to _data folder ..."
+cp "results/timeline-releases-only.yml" "../../_data/timeline-releases-only.yml"
