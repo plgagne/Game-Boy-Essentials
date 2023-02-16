09c32c7c244501a41b0727937f0de352174ff914
plgagne <pierrelucgagne@icloud.com>
2023-02-16 17:36:54 +0000

    séparer les ids dans deux fichiers

--- timeline/mobygames.sh
+++ timeline/mobygames.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# MobyGames API Docs
+# https://www.mobygames.com/info/api
+
 # MobyGames API Key
 key=vC7/9vmj7RGEMdT14uf3Aw==
 
@@ -17,33 +20,64 @@ hourly_rate_limit=360
 RED='\033[0;31m'
 
 platform_list_fetch() {
-    curl -k --location --request GET "https://api.mobygames.com/v1/platforms?api_key=$key" | jq '.' > "platforms.json"
+    file="temp/platforms.json"
+    rm $file
+    curl -k --location --request GET "https://api.mobygames.com/v1/platforms?api_key=$key" | jq '.' > $file
+    sleep 2
+    jsonlint -q $file
 }
 
-game_list_fetch() {
-    rm "games.json"
+gb_gbc_ids_fetch() {
+
+    # GB
     platform=$game_boy
-    for x in {0..1}
-    do
-        echo Doing platform number:$platform
-        echo $offset
-        for i in {0..8}
+    file="temp/game_boy_ids.json"
+    rm $file
+    game_ids_fetch
+    
+    # GBC
+    platform=$game_boy_color
+    file="temp/game_boy_color_ids.json"
+    rm $file
+    game_ids_fetch
+}
+  
+game_ids_fetch() {   
+    echo "${RED}Doing platform number:$platform"
+    echo "${RED}$offset"
+    for i in {0..8}
         do
-            curl -k --location --request GET "https://api.mobygames.com/v1/games?platform=$platform&format=id&offset=$offset&api_key=$key" | jq '.' >> "games.json"
+            curl -k --location --request GET "https://api.mobygames.com/v1/games?api_key=$key&platform=$platform&format=id&offset=$offset" | jq '.' >> $file
             let "offset+=100"
             echo $offset
             sleep 2
         done
-        offset=0
-        platform=$game_boy_color
+    offset=0
+        
+    # Make valid JSON 
+    jq -s 'map(.[][])' $file > INPUT.tmp && mv INPUT.tmp $file
+    jsonlint -q $file
+}
+
+games_fetch() {
+    file="temp/games.json"
+    rm $file
+    jq -r '.[]' "temp/game_ids.json" | while read id; do
+        curl -k --location --request GET "https://api.mobygames.com/v1/games?api_key=$key&format=normal&id=$id" | jq '.' >> $file
+        sleep 2
     done
-    jq -s 'map(.[][])' "games.json" > INPUT.tmp && mv INPUT.tmp "games.json"
-    sed "[" "" > "games.json""
+    jsonlint -q $file
 }
 
-specific_game_fetch() {
-    curl -k --location --request GET "https://api.mobygames.com/v1/games/141/platforms/10?api_key=$key" | jq '.' > "specific.json"
-    
+platform_release_fetch() {
+    platform=$game_boy
+    file="temp/releases.json"
+    rm $file
+    jq -r '.[]' "temp/game_ids.json" | while read id; do
+        curl -k --location --request GET "https://api.mobygames.com/v1/games/$id/platforms/$platform?api_key=$key" | jq '.' >> $file
+        sleep 2
+    done
+    jsonlint -q $file
 }
 
-game_list_fetch
+gb_gbc_ids_fetch
