2bf0ffd3ec2585690860ef1377fb072395df402e
Pierre-Luc Gagné <plgagne@portable-pl.local>
2025-04-16 00:04:04 -0400

    déplacer les corrections et les retraits

--- timeline/timeline-finalizer.rb
+++ timeline/timeline-finalizer.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/ruby
 require "json"
+require "yaml"
+
 # Open json file
 timeline_na = JSON.load_file 'temp/timeline.json'
+corrections = YAML.load_file 'corrections.yml'
 
 # Add developer to each release (AI written code)
 timeline_na['games'].each do |game|
@@ -16,6 +19,9 @@ timeline_na['games'].each do |game|
   end
 end
 
+# Sort
+timeline_na["games"] = timeline_na["games"].sort_by { |entry| entry["representative_name"] }
+
 File.write('results/timeline-complete.json', JSON.pretty_generate(timeline_na))
 
 # Extract all releases
@@ -24,9 +30,38 @@ result = timeline_na["games"].find_all { |h1| h1['representative_name']}.map { |
 
 # Remove all depth to the array
 result = result.flatten
+# Remove empty objects
 result.compact!
 
 # Sort
 sorted_result = result.sort_by { |game| game['release_date'] }
 
+# Corrections (AI written code)
+corrections.each do |correction|
+  key = correction['key']
+  value = correction['value']
+  if correction.key?('updates')
+    sorted_result.each do |entry|
+      if entry[key].to_s == value.to_s
+        correction['updates'].each do |update_key, new_value|
+          entry[update_key] = new_value
+        end
+      end
+    end
+  else
+    before = sorted_result.size
+    sorted_result.reject! { |entry| entry[key].to_s == value.to_s }
+    after = sorted_result.size
+  end
+end
+
+# Working sort
+sorted_result_nocoverontop = sorted_result.sort_by do |entry|
+  [
+    entry['boxart'] == '/no_cover.png' ? 0 : 1,
+    entry['market']
+  ]
+end
+
 File.write('results/timeline-releases.json', JSON.pretty_generate(sorted_result))
+File.write('results/timeline-releases-nocoverontop.json', JSON.pretty_generate(sorted_result_nocoverontop))
